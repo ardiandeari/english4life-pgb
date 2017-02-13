06a36aae3c93fd3c4df669f9dc6eb8d86c5fe893
ardiandeari <ardi.deari@gmail.com>
2017-02-13 13:54:42 +0100

    a

--- js/controllers.js
+++ js/controllers.js
@@ -17,24 +17,3 @@ angular.module('APP.controllers', [])
 .controller('ForumCtrl', function($scope) {
 
 });
-
-import { Component } from '@angular/core';
-import { Camera } from 'ionic-native';
-@Component({
- selector: 'Forum',
- templateUrl: 'templates/tab-forum.html'
-})
-export class Forum {
-  base64Image
-  constructor() {}
- accessGallery(){
-   Camera.getPicture({
-     sourceType: Camera.PictureSourceType.SAVEDPHOTOALBUM,
-     destinationType: Camera.DestinationType.DATA_URL
-    }).then((imageData) => {
-      this.base64Image = 'data:image/jpeg;base64,'+imageData;
-     }, (err) => {
-      console.log(err);
-    });
-  }
-}
